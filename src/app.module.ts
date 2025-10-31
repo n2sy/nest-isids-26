37feea601e1fae2c35e3e0e5712034e90735aee0
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-10-31 10:39:45 +0100

    séance 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  RequestMethod,
+  type MiddlewareConsumer,
+  type NestModule,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FirstModule } from './first/first.module';
 import { TasksModule } from './tasks/tasks.module';
+import { FirstMiddleware } from './middlewares/first/first.middleware';
+import { SecondMiddleware } from './middlewares/second/second.middleware';
 
 @Module({
   imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(FirstMiddleware).forRoutes(''); // appliqué pour toutes les routes
+    // consumer.apply(FirstMiddleware).forRoutes('tasks/*'); // appliqué pour toutes les routes
+    consumer.apply(SecondMiddleware).forRoutes('');
+    consumer.apply(FirstMiddleware).forRoutes({
+      path: 'tasks/*',
+      method: RequestMethod.GET,
+    });
+  }
+}
